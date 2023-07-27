cc7d665eeabe231551dc26eb80dd71dfa7fc3f1e
J0ke <cs.gamer0902@gmail.com>
2023-07-27 12:11:12 +0300

    second commit hw6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    public void throwExceptions() throws Exception {
+        try {
+            throw new Exception("Exception in try block");
+        } catch (Exception e) {
+            throw new Exception("Exception in catch block");
+        } finally {
+            throw new Exception("Exception in finally block");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main example = new Main();
+        try {
+            example.throwExceptions();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
