9a969d9be0b789b261c40f6bb5d438d2e10779f0
J0ke <cs.gamer0902@gmail.com>
2023-07-27 12:59:16 +0300

    first commit hw7

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
+class CheckedException extends Exception {
+    public CheckedException(String message) {
+        super(message);
+    }
+}
+class UncheckedException extends RuntimeException {
+    public UncheckedException(String message) {
+        super(message);
+    }
+}
 public class Main {
-    public void throwExceptions() throws Exception {
+    public static void main(String[] args) {
         try {
-            throw new Exception("Exception in try block");
-        } catch (Exception e) {
-            throw new Exception("Exception in catch block");
-        } finally {
-            throw new Exception("Exception in finally block");
+            throwCheckedException();
+        } catch (CheckedException e) {
+            System.out.println("Проверяемое исключение: " + e.getMessage());
+        }
+
+        try {
+            throwUncheckedException();
+        } catch (UncheckedException e) {
+            System.out.println("Непроверяемое исключение: " + e.getMessage());
         }
     }
-    public static void main(String[] args) {
-        Main example = new Main();
-        try {
-            example.throwExceptions();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public static void throwCheckedException() throws CheckedException {
+        throw new CheckedException("Проверяемое исключение");
+    }
+    public static void throwUncheckedException() {
+        throw new UncheckedException("Непроверяемое исключение");
     }
 }
