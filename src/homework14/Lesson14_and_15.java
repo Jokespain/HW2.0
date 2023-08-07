24f12072029eaebcdc5a625572d545a79da36337
J0ke <cs.gamer0902@gmail.com>
2023-08-07 08:31:54 +0300

    hw14

---
+++ src/homework14/Lesson14_and_15.java
@@ -0,0 +1,18 @@
+package homework14;
+
+public class Lesson14_and_15 {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Роман";
+        person1.age = 50;
+        System.out.println("Меня зовут " + person1.name + "," + "мне " + person1.age + "лет");
+        Person person2 = new Person();
+        person2.name = "Вова";
+        person2.age = 20;
+        System.out.println("Меня зовут " + person2.name + "," + "мне " + person2.age + "лет");
+        // Lesson 15
+        System.out.println();
+        person2.speak();
+        person2.sayHello();
+    }
+}
